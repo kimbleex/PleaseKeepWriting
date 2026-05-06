6e44be9d8eb0e7cc91fd109bbb60da5f7c410a82
xjl <kimbleex@outlook.com>
2026-05-06 15:31:15 +0800

    faster menu jump

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  prefetch: true,
   vite: {
     ssr: {
       external: ['@prisma/client']
